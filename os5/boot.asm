4f3f69b0f7c7b2c78ba2c3c87ad0b9cfc82c888b
astralorchid <61172763+astralorchid@users.noreply.github.com>
2020-11-09 11:40:12 -0500

    Add files via upload

---
+++ os5/boot.asm
@@ -0,0 +1,31 @@
+[org 0x7c00]
+xor ax, ax
+mov ds, ax
+
+mov ax, 0x9000
+mov ss, ax
+
+mov bp, 0xFFFF
+mov sp, bp
+
+push ds
+push start
+ret
+
+start:
+    mov ah, 0x02 ;read
+    mov al, 0x04 ;#sectors
+    mov ch, 0 ;cyl
+    mov cl, 0x02 ;start from sector
+    mov dh, 0x00 ;head
+    mov dl, 0x00 ;drive
+    mov bx, 0x7e00 ;offset dest
+
+    int 0x13
+
+    jmp 0x7e00
+jmp $
+
+times 510-($-$$) db 0
+db 0x55
+db 0xAA
---
+++ os5/kernel.asm
@@ -0,0 +1,118 @@
+[org 0x7e00]
+xor ax, ax
+mov ds, ax
+
+call getInitVideoMode
+call setInitVideoMode
+call clearScreen
+
+mov ax, 0
+mov dx, ax
+xor cx, cx
+xor bx, bx
+call hprint
+call newLine
+call newProgram
+call newProgram
+jmp $
+newProgram:
+    call pmalloc
+
+    mov es, ax
+
+    mov ah, 0x02 ;read
+    mov al, 0x04 ;#sectors
+    mov ch, 0x00 ;cyl
+    mov cl, 0x01 ;start from sector
+    mov dh, 0x00 ;head
+    mov dl, 0x01 ;drive
+    mov bx, 0x0000 ;offset dest
+    int 0x13
+
+
+
+    push ds ;save jmp segment
+    push fromProgram
+
+    mov ax, es
+    mov ds, ax
+
+    push es
+    push bx
+    retf
+
+    fromProgram:
+        mov ax, 0
+        mov ds, ax
+
+        mov si, msg
+        call sprint
+        call newLine
+    ret
+    
+
+
+
+pmalloc:
+    mov ax, [loopSeg]
+    push ax ;save segment
+    cmp ax, [maxSeg]
+    je .segDone
+    
+    mov es, ax
+    mov bx, [es:0x0000]
+
+    cmp bx, 0x70
+    je .hasProgram
+    mov si, freespaceMsg
+    call sprint
+
+    mov ax, es
+    mov dx, ax
+    xor cx, cx
+    xor bx, bx
+    call hprint
+    call newLine
+
+    pop ax
+    ;add ax, [segmentSize]
+    ;mov [loopSeg], ax
+    ;jmp pmalloc
+
+    mov bx, [minSeg]
+    mov [loopSeg], bx
+    ret
+
+    .hasProgram:
+    mov si, pallocMsg
+    call sprint
+
+    mov ax, [loopSeg]
+    mov dx, ax
+    xor cx, cx
+    xor bx, bx
+    call hprint
+    call newLine
+
+    pop ax
+    add ax, [segmentSize]
+    mov [loopSeg], ax
+    jmp pmalloc
+
+    .segDone:
+    pop ax
+ret
+
+%include 'print16.asm'
+msg db 'returned to kernel', 0
+pallocMsg db 'Program allocated at ', 0
+freespaceMsg db 'Free segment at ', 0
+
+segmentSize dw 0x1000
+minSeg dw 0x1000
+maxSeg dw 0x8000
+loopSeg dw 0x1000
+
+freeSeg dw 0
+
+times 2048-($-$$) db 0
