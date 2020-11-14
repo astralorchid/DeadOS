0e886d8da0b79b1c078374f6c1dc66e0a96771d9
astralorchid <61172763+astralorchid@users.noreply.github.com>
2020-11-14 17:51:59 -0500

    Add files via upload

--- os5/kernel.asm
+++ os5/kernel.asm
@@ -6,6 +6,15 @@ call getInitVideoMode
 call setInitVideoMode
 call clearScreen
 
+cli
+    mov al, 0x00
+    out 0xa1, al
+    out 0x21, al
+sti
+
+mov [ds:irq1_ivt], word readChar 
+mov [ds:irq1_ivt+2], word 0x00
+
 call newProgram
 
 jmp $
@@ -23,8 +32,6 @@ newProgram:
     mov dl, 0x01 ;drive
     mov bx, 0x0000 ;offset dest
     int 0x13
-
-
 
     push ds ;save jmp segment
     push fromProgram
@@ -73,8 +80,31 @@ pmalloc:
     pop ax
 ret
 
+
+readChar:
+    push ax
+
+    in al, 01100000b
+
+    test al, 10000000b
+    jnz .inputEnd
+
+    mov ah, 0
+    mov dx, ax
+    xor cx, cx
+    xor bx, bx
+    call hprint
+    call newLine
+
+    .inputEnd:
+        mov al, 01100001b
+        out 0x20, al
+    pop ax
+iret
+
 %include 'print16.asm'
 msg db 'returned to kernel', 0
+keyPressStr db 'Key Press', 0
 pallocMsg db 'Program allocated at ', 0
 freespaceMsg db 'Free segment at ', 0
 
@@ -84,5 +114,6 @@ maxSeg dw 0x8000
 loopSeg dw 0x1000
 
 freeSeg dw 0
+irq1_ivt equ 0x0024
 
 times 2048-($-$$) db 0
