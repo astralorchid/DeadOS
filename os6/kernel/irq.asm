74126c4ddfca9a12b162e048fda2439cba67d91c
astralorchid <61172763+astralorchid@users.noreply.github.com>
2020-11-19 23:31:13 -0500

    Add files via upload

--- os6/kernel/irq.asm
+++ os6/kernel/irq.asm
@@ -10,13 +10,15 @@ irq:
             out 0x40, al
 
             ;set kernel ivt
-            mov [ds:irq0_ivt], word .irq0
-            mov [ds:irq0_ivt+2], word 0x00
+            call irq.map_irq0
+            call irq.map_irq1
 
-            mov [ds:irq1_ivt], word .irq1
-            mov [ds:irq1_ivt+2], word 0x00
+            mov bl, [IRQ_MASKS]
+            call .ENABLE_IRQx
+            mov bl, [IRQ_MASKS+1]
+            call .ENABLE_IRQx
 
-            mov al, 00000000b ;enable pic
+            mov al, [IRQ_FLAGS] ;enable pic
             out 0xa1, al
             out 0x21, al
         sti
@@ -31,16 +33,46 @@ irq:
 
     .irq0:
         pusha
-            ;mov ah, 0x0e
-            ;mov al, byte 'e'
-            ;int 0x10
-            
+            mov ah, 0x0e
+            mov al, byte 'e'
+            int 0x10
+
             mov al, 0x20
-            out 0x20, al  
+            out PIC0, al  
         popa
     iret
 
-    .disable_irq0:
+    .map_irq0:
+        mov [ds:irq0_ivt], word .irq0
+        mov [ds:irq0_ivt+2], word 0x00
+    ret
+
+    .ENABLE_IRQx:
+        push ax
+        push bx
+
+        mov al, [IRQ_FLAGS]
+        xor al, bl
+
+        mov [IRQ_FLAGS], al
+
+        pop bx
+        pop ax
+    ret
+
+    ;mov bl, IRQx_DISABLE_MASK
+    .DISABLE_IRQx:
+        push ax
+        push bx
+
+        mov al, [IRQ_FLAGS]
+        or al, bl
+        
+        mov [IRQ_FLAGS], al
+
+        pop bx
+        pop ax
+    ret
 
     .irq1:
         push ax
@@ -62,15 +94,25 @@ irq:
 
         .inputEnd:
             mov al, 01100001b
-            out 0x20, al
+            out PIC0, al
         pop ax
     iret
+
+    .map_irq1:
+        mov [ds:irq1_ivt], word .irq1
+        mov [ds:irq1_ivt+2], word 0x00
+    ret
 
 %include '../keymap.asm'
 irq0_ivt equ 0x0020
 irq1_ivt equ 0x0024
 
-PIC0 equ 0x20
-PIC1 equ 0xa0
-PIC0_COM equ 0x20
-PIC1_COM equ 0xa0
+IRQ_MASKS:
+    db 00000001b
+    db 00000010b
+
+IRQ_FLAGS:
+    db 01111111b
+
+PIC0 equ 0x20 ;also 
+PIC1 equ 0xa0 ;command ports
