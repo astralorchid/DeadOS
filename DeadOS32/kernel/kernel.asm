fb4d295eae334201b0d1a678ebcb592887aebe9e
astralorchid <61172763+astralorchid@users.noreply.github.com>
2021-02-08 21:55:04 -0500

    Add files via upload

--- DeadOS32/kernel/kernel.asm
+++ DeadOS32/kernel/kernel.asm
@@ -8,41 +8,55 @@ DisableNMI:
 
 OpenA20:
     in al, 0xD0
-    or al, 10b
+    or al, 2
     out 0xD1, al ;open a20
+    in al, 0x92
+    or al, 2
+    out 0x92, al ;open a20
 
-cli
-lgdt [GDT_TABLE_ENTRY]
-mov eax, cr0
-or al, 1
-mov cr0, eax
+LoadInitalGDT:
+    cli
+    lgdt [GDT_DESCRIPTOR]
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
 
-mov ax, 0x08
-mov ds, ax
-jmp start32
+    jmp CODESEG:start32
 
-dq 0
-dd 0
-dw 0
-GDT_TABLE_ENTRY:
-    dw 0 ;base 0:15
-    dw 0xFFFF ;limit 0:15
-    db 0x01 ;base 24:31
+GDT_NULL_DESC:
+    dd 0
+    dd 0
+GDT_CODE_ENTRY:
+    dw 0xFFFF ;LIMIT 0:15
+    dw 0x0000 ;BASE 0:15
+    db 0x00 ;BASE 16:23
+    db 10011010b ;ACCESS BYTE
+    db 01001111b ;LOW BITS LIMIT 16:19, HIGH BITS FLAGS
+    db 00 ;BASE 24:31
+GDT_DATA_ENTRY:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
     db 01001111b
-    db 10001010b
-    db 0
+    db 00
+GDT_END:
+
+GDT_DESCRIPTOR:
+    dw GDT_END - GDT_NULL_DESC - 1
+    dd GDT_NULL_DESC
+
+CODESEG equ GDT_CODE_ENTRY - GDT_NULL_DESC
+DATASEG equ GDT_DATA_ENTRY - GDT_NULL_DESC
 
 [bits 32]
 
 start32:
-mov ax, 0x0
+mov ax, DATASEG
 mov ds, ax
 mov es, ax
 mov fs, ax
 mov gs, ax
 mov ss, ax
 
-mov ebx, dword 0xb800
-mov al, byte 'E'
-mov byte [ebx], al
 jmp $
